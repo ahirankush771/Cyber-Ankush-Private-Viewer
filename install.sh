9e6eebe10ac48316e1af16e9792dbb28c08239c2
Ankush Kumar <ahirankush771@gmail.com>
2026-02-21 13:07:54 +0530

    Update installation script to use new repository link

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo -e "\e[1;32m🔥 Installing Cyber_Ankush Private Viewer...\e[0m"
-curl -LO https://raw.githubusercontent.com/cyber_ankush/Cyber-Ankush-Private-Viewer/main/cyber_ankush_viewer.sh
+curl -LO https://raw.githubusercontent.com/ahirankush771/Cyber-Ankush-Private-Viewer/main/cyber_ankush_viewer.sh
 chmod +x cyber_ankush_viewer.sh
 echo -e "\e[1;32m✅ Installed! Run:\e[0m ./cyber_ankush_viewer.sh"
-echo -e "\e[1;36mOr one-click:\e[0m bash <(curl -s [YOUR_GITHUB_LINK]/install.sh)"
